4a31ff92eab2ae167c7e1a98f48dff767b77936b
Евгений Куликов <foxkait@yandex.ru>
2022-05-26 14:24:19 +0500

    Версия 4

--- index.js
+++ index.js
@@ -1,53 +1,50 @@
 const express = require('express');
 const { Client } = require("pg");
 const serverless = require('serverless-http');
-const app = express();
-const tableName = "backend";
 
-const createClient = (request) => {
-	const proxyId = "akf1ca19r8vbi37sutha";
-	const proxyEndpoint = "akf1ca19r8vbi37sutha.postgresql-proxy.serverless.yandexcloud.net";
+const createClient = (token) => {
+	const proxyId = "akfeee9p1eubbfg533h4";
+	const proxyEndpoint = "akfeee9p1eubbfg533h4.postgresql-proxy.serverless.yandexcloud.net:6432";
 	const user = "user1";
-	const connection = `postgres://${user}:${request.apiGateway.context.token.access_token}@${proxyEndpoint}:6432/${proxyId}?ssl=true`;
-	const client = new Client(connection);
+	const conString = "postgres://" + user + ":" + token + "@" + proxyEndpoint + "/" + proxyId + "?ssl=true";
+	const client = new Client(conString);
 	client.connect();
 	return client;
 }
 
-app.get('/lists', async (request, response) => {
-	const client = createClient(request)
-	let result = await client.query(`SELECT * FROM ${tableName}`);
-	response.send([...result.rows]);
+const app = express();
+
+app.get('/lists', async (req, res) => {
+	const client = createClient(req.apiGateway.context.token.access_token)
+	let result = await client.query("SELECT * FROM data");
+	res.send([...result.rows]);
 });
 
-app.post('/lists', async (request, response) => {
-	response.setHeader('Access-Control-Allow-Origin', '*');
-	response.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
-	const client = createClient(request);
-	const body = JSON.parse(Buffer.from(request.body, 'base64').toString());
-
-	await client.query(`INSERT INTO ${tableName} (id, name, boughtProducts, pendingProducts) VALUES(${body.id}, '${body.name}', ARRAY[]::INT[], ARRAY[]::INT[])`);
-	response.send({ ...request.body });
+app.post('/lists', async (req, res) => {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
+	const body = JSON.parse(Buffer.from(req.body, 'base64').toString())
+	const client = createClient(req.apiGateway.context.token.access_token)
+	await client.query(`INSERT INTO data (id, name, boughtProducts, pendingProducts) VALUES(${body.id}, '${body.name}', ARRAY[]::INT[], ARRAY[]::INT[])`);
+	res.send({ ...req.body });
 });
 
-app.delete('/lists', async (request, response) => {
-	response.setHeader('Access-Control-Allow-Origin', '*');
-	response.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
-	const client = createClient(request);
-	const body = JSON.parse(Buffer.from(request.body, 'base64').toString());
-
-	await client.query(`DELETE FROM ${tableName} WHERE id='${body.id}'`);
-	response.send({ ...request.body });
+app.post('/lists/delete', async (req, res) => {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
+	const body = JSON.parse(Buffer.from(req.body, 'base64').toString())
+	const client = createClient(req.apiGateway.context.token.access_token)
+	await client.query(`DELETE FROM data WHERE id='${body.id}'`);
+	res.send({ ...req.body });
 });
 
-app.put('/lists', async (request, response) => {
-	response.setHeader('Access-Control-Allow-Origin', '*');
-	response.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
-	const client = createClient(request);
-	const body = JSON.parse(Buffer.from(request.body, 'base64').toString());
-
-	await client.query(`UPDATE ${tableName} SET name = '${body.name}' WHERE id = ${body.id}`);
-	response.send({ ...request.body });
+app.post('/lists/put', async (req, res) => {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
+	const body = JSON.parse(Buffer.from(req.body, 'base64').toString())
+	const client = createClient(req.apiGateway.context.token.access_token)
+	await client.query(`UPDATE data SET name = '${body.name}' WHERE id = ${body.id}`);
+	res.send({ ...req.body });
 });
 
 module.exports.handler = serverless(app);
